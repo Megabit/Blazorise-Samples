c2985470953a0b59fcbc9671249d35e843f4e10e
James Amattey <me@jamattey.com>
2022-08-12 12:26:33 +0000

    Login Page Demo (#3)
    * Login page ui demo
    
    * Defined Blazorise Components
    
    * Recreated the counter page using Blazorise Components
    
    * new commit
    
    * Created Login Page
    
    * Added Login page to nav menu
    
    * Define SelectedTab variable
    
    * fix errors
    
    * Added Validation To Email
    
    * Added Method Handler Validation
    
    * fix errors
    
    * place form into cards
    
    * place forms into container
    
    * Add manual validation
    
    * Implemented changes
    
    * new commit
    
    * Fix validations
    
    * Use OnSelectedTabChanged
    
    Co-authored-by: Mladen Macanovic <mladen.macanovic@gmail.com>

---
+++ LoginPageDemo/Client/Program.cs
@@ -0,0 +1,21 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Login_Page_Demo;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+builder.Services
+    .AddBlazorise( options =>
+    {
+        options.Immediate = true;
+    } )
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
+
+await builder.Build().RunAsync();
