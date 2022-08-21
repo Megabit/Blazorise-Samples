5fff0e346d182aa7669fe2a8e76b83d9d86fea7b
James Amattey <me@jamattey.com>
2022-08-21 01:37:29 +0000

    Added configuration files

--- BlazorWasmAuthentication/Program.cs
+++ BlazorWasmAuthentication/Program.cs
@@ -1,11 +1,24 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWasmAuthentication;
+
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddBlazorise( options =>
+     {
+         options.Immediate = true;
+     })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
