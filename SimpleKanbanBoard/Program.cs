c37da1ac44d50c0f6539a766fcbbe7343da23ea2
James Amattey <me@jamattey.com>
2022-09-05 04:24:47 +0000

    Update Program.cs
    Added Blazorise builder services

--- SimpleKanbanBoard/Program.cs
+++ SimpleKanbanBoard/Program.cs
@@ -1,5 +1,6 @@
 using Blazorise;
 using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SimpleKanbanBoard;
@@ -10,11 +11,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-.AddBlazorise(options =>
- {
-     options.Immediate = true;
- })
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
     .AddBootstrapProviders()
     .AddFontAwesomeIcons();
+
 
 await builder.Build().RunAsync();
