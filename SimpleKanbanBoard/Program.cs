61c45e770fe66c88ee6785301215ad4e27583eb9
James Amattey <me@jamattey.com>
2022-09-05 04:21:56 +0000

    Initial Commit

--- SimpleKanbanBoard/Program.cs
+++ SimpleKanbanBoard/Program.cs
@@ -1,3 +1,5 @@
+using Blazorise;
+using Blazorise.Bootstrap;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SimpleKanbanBoard;
@@ -7,5 +9,12 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+.AddBlazorise(options =>
+ {
+     options.Immediate = true;
+ })
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
